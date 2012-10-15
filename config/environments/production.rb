1a58490ede774e30c1003e7b9d6244bfa76987dc
shaunoconnor <shaun@shaun-oconnor.com>
2012-10-15 14:16:02 +1100

    Update assets serving and humans.txt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 ShaunOConnorBlog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  # config.middleware.insert_before 0, 'SamSoffes::PreferredHost'
+  # config.middleware.insert_before 0, 'ShaunOConnorBlog::PreferredHost'
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -21,7 +21,7 @@ ShaunOConnorBlog::Application.configure do
   #end
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true

b7a1a1b6d92ca100e804d33849b9cb18c149b052
Shaun O'Connor <shaun@shaun-oconnor.com>
2014-01-29 09:07:03 +1100

    Disable asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module ShaunOConnorBlog
     config.active_record.identity_map = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0.3'

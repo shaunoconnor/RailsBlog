26c96800776e58b77ee00968970f47d749a17a9e
Shaun O'Connor <shaun@shaun-oconnor.com>
2014-02-20 11:28:09 +1100

    Update asset sync initialiser

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,23 +1,26 @@
-AssetSync.configure do |config|
+if defined?(AssetSync)
 
-  config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  config.fog_directory = ENV['FOG_DIRECTORY']
+  AssetSync.configure do |config|
 
-  # Increase upload performance by configuring your region
-  config.fog_region = 'ap-southeast-2'
+    config.fog_provider = 'AWS'
+    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    config.fog_directory = ENV['FOG_DIRECTORY']
 
-  # Don't delete files from the store
-  config.existing_remote_files = "keep"
+    # Increase upload performance by configuring your region
+    config.fog_region = 'ap-southeast-2'
 
-  # Automatically replace files with their equivalent gzip compressed version
-  config.gzip_compression = true
+    # Don't delete files from the store
+    config.existing_remote_files = "keep"
 
-  # Use the Rails generated 'manifest.yml' file to produce the list of files to
-  # upload instead of searching the assets directory.
-  config.manifest = true
+    # Automatically replace files with their equivalent gzip compressed version
+    config.gzip_compression = true
 
-  # Fail silently.  Useful for environments such as Heroku
-  config.fail_silently = true
+    # Use the Rails generated 'manifest.yml' file to produce the list of files to
+    # upload instead of searching the assets directory.
+    config.manifest = true
+
+    # Fail silently.  Useful for environments such as Heroku
+    config.fail_silently = true
+  end
 end
